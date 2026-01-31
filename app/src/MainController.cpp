99322f90150172191320c784bcc5b1a6872949d1
Nemanja Badric <nemanjabadric@gmail.com>
2026-01-31 20:13:25 +0100

    Implemented closing app on ESC key.

--- app/src/MainController.cpp
+++ app/src/MainController.cpp
@@ -4,11 +4,18 @@
 
 #include "../include/MainController.hpp"
 
+#include <engine/platform/PlatformController.hpp>
+
 namespace app {
     void MainController::initialize() {
     }
 
     bool MainController::loop() {
+        auto platform = engine::core::Controller::get<engine::platform::PlatformController>();
+        if (platform->key(engine::platform::KeyId::KEY_ESCAPE).is_down()) {
+            return false;
+        }
+        return true;
     }
 
     void MainController::update() {
