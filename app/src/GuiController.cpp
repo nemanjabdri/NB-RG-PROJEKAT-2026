deabbf1eff4a4ca46f57672b3f67a742f296c699
Nemanja Badric <nemanjabadric@gmail.com>
2026-02-03 15:41:48 +0100

    Implemented basic GUI that shows camera position.

---
+++ app/src/GuiController.cpp
@@ -0,0 +1,41 @@
+//
+// Created by nemanja on 3.2.26..
+//
+
+#include "../include/GuiController.hpp"
+
+#include <imgui.h>
+#include <engine/graphics/GraphicsController.hpp>
+#include <engine/platform/PlatformController.hpp>
+
+namespace engine::platform {
+    class PlatformController;
+}
+
+namespace app {
+    void GuiController::initialize() {
+        set_enable(true);
+    }
+
+    void GuiController::poll_events() {
+        auto platform = engine::core::Controller::get<engine::platform::PlatformController>();
+
+        if (platform->key(engine::platform::KEY_F2).state() == engine::platform::Key::State::JustPressed) {
+            set_enable(!is_enabled());
+        }
+    }
+
+    void GuiController::draw() {
+        auto graphics = engine::core::Controller::get<engine::graphics::GraphicsController>();
+        auto camera   = graphics->camera();
+        graphics->begin_gui();
+
+        ImGui::Begin("Camera info. ");
+
+        ImGui::Text("Camera position: (%f %f %f)", camera->Position.x, camera->Position.y, camera->Position.z);
+
+        ImGui::End();
+
+        graphics->end_gui();
+    }
+} // app
--- app/src/MyApp.cpp
+++ app/src/MyApp.cpp
@@ -4,6 +4,7 @@
 
 #include "../include/MyApp.hpp"
 
+#include <GuiController.hpp>
 #include <MainController.hpp>
 #include <spdlog/spdlog.h>
 
@@ -11,6 +12,8 @@ namespace app {
     void MyApp::app_setup() {
         spdlog::info("App setup completed!");
         auto mainController = register_controller<app::MainController>();
+        auto guiController  = register_controller<app::GuiController>();
         mainController->after(engine::core::Controller::get<engine::core::EngineControllersEnd>());
+        mainController->before(guiController);
     }
 } // app
